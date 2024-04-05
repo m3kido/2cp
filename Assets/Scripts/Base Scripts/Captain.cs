52366475d8bb051796cb24b7a1e967e14399250d
Redvirex <113212115+Redvirex@users.noreply.github.com>
2024-04-05 14:07:39 +0100

    smth

--- Assets/Scripts/Base Scripts/Captain.cs
+++ Assets/Scripts/Base Scripts/Captain.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 
-public class Captain : MonoBehaviour
+public abstract class Captain
 {
     public CaptainData Data;
+    public bool IsCelesteActive = false;
+    public Player Player;
+
 
     public virtual void ApplySpecial()
     {
 
+    }
+
+    public virtual bool IsCelesteReady()
+    {
+        return false;
     }
 }
---
+++ Assets/Scripts/Managers/CaptainManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+public class CaptainManager : MonoBehaviour
+{
+    GameManager Gm;
+
+    private void Awake()
+    {
+        Gm = FindObjectOfType<GameManager>();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
